1356baf64902d49be27185492c2c7095786b83de
m2achilles-web <m2achilles@gmail.com>
2026-09-08 02:13:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Supabase İstemci Yapılandırması (Kendi URL ve Anon Key bilgilerini buraya eklediğinden emin ol)
-const SUPABASE_URL = 'https://wcjusyzrlnnbtwyjypnm.supabase.co/rest/v1/';
+const SUPABASE_URL = 'https://wcjusyzrlnnbtwyjypnm.supabase.co/';
 const SUPABASE_ANON_KEY = 'sb_publishable_K2AIrHSs765CUXlGzqlCdg_ntTpKVXi';
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
